9bc9d7fb88af5c6b94840186c078207e1a52e619
Ruixi Zhou <zhouruixi@gmail.com>
2025-01-29 19:26:01 +0000

    合并加载/保存majestic/wfb的js代码
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- static/js/configurator.js
+++ static/js/configurator.js
@@ -23,14 +23,14 @@ $(document).ready(function () {
                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                 加载配置成功！
                                 </div>`);
-            // 设置 3 秒后自动消失
+            // 设置 2 秒后自动消失
             setTimeout(function () {
                 $('#load-result-gs-success-alert').alert('close');
             }, 2000);
             // resultDiv.html(`<div class="alert alert-success">读取配置成功！</div>`);
             // alert("加载配置成功！");
         }).fail(function () {
-            alert("加载 gs 配置失败！");
+            alert("加载 gs 配置失败，请手动重新加载！");
         });
         // }
     }
@@ -57,7 +57,7 @@ $(document).ready(function () {
                                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                         ${response.message}
                                         </div>`);
-                    // 设置 3 秒后自动消失
+                    // 设置 2 秒后自动消失
                     setTimeout(function () {
                         $('#save-result-gs-success-alert').alert('close');
                     }, 2000);
@@ -72,11 +72,10 @@ $(document).ready(function () {
         // }
     }
 
-    // 加载 Drone wfb 配置
-    function loadDroneWfbConfig() {
-        // if ($("#drone-config-wfb").hasClass("active")) {
-        $.get("/load_drone_config/wfb", function (data) {
-            const container = $("#drone-config-container-wfb");
+    // 加载 Drone 配置
+    function loadDroneConfig(config_name) {
+        $.get(`/load_drone_config/${config_name}`, function (data) {
+            const container = $(`#drone-config-container-${config_name}`);
             container.empty(); // 清空容器
 
             // 动态生成配置表单
@@ -95,26 +94,24 @@ $(document).ready(function () {
                     container.append(row);
                 }
             }
-            const resultDiv = $("#save-result-drone-wfb");
-            resultDiv.html(`<div class="alert alert-success alert-dismissible fade show" role="alert" id="load-result-drone-wfb-success-alert">
+            const resultDiv = $(`#save-result-drone-${config_name}`);
+            resultDiv.html(`<div class="alert alert-success alert-dismissible fade show" role="alert" id="load-result-drone-${config_name}-success-alert">
                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                 加载配置成功！
                                 </div>`);
-            // 设置 3 秒后自动消失
+            // 设置 2 秒后自动消失
             setTimeout(function () {
-                $('#load-result-drone-wfb-success-alert').alert('close');
+                $(`#load-result-drone-${config_name}-success-alert`).alert('close');
             }, 2000);
             // resultDiv.html(`<div class="alert alert-success">读取配置成功！</div>`);
             // alert("加载配置成功！");
         }).fail(function () {
-            alert("加载 Drone wfb 配置失败，请手动重新加载！");
+            alert(`加载 Drone ${config_name} 配置失败，请手动重新加载！`);
         });
-        // }
     }
 
-    // 保存 Drone wfb配置
-    function saveDroneWfbConfig() {
-        // if ($("#drone-config-wfb").hasClass("active")) {
+    // 保存 Drone 配置
+    function saveDroneConfig(config_name) {
         const data = {};
         $(".config-input").each(function () {
             const key = $(this).data("key");
@@ -123,104 +120,27 @@ $(document).ready(function () {
         });
 
         $.ajax({
-            url: "/save_drone_config/wfb",
+            url: `/save_drone_config/${config_name}`,
             method: "POST",
             contentType: "application/json",
             data: JSON.stringify(data),
             success: function (response) {
-                const resultDiv = $("#save-result-drone-wfb");
+                const resultDiv = $(`#save-result-drone-${config_name}`);
                 if (response.success) {
-                    resultDiv.html(`<div class="alert alert-success alert-dismissible fade show" role="alert" id="save-result-drone-wfb-success-alert">
-                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                        ${response.message}
-                                        </div>`);
-                    // 设置 3 秒后自动消失
+                    resultDiv.html(`<div class="alert alert-success alert-dismissible fade show" role="alert" id="save-result-drone-${config_name}-success-alert">
+                                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                            ${response.message}
+                                            </div>`);
+                    // 设置 2 秒后自动消失
                     setTimeout(function () {
-                        $('#save-result-drone-wfb-success-alert').alert('close');
+                        $(`#save-result-drone-${config_name}-success-alert`).alert('close');
                     }, 2000);
                 } else {
                     resultDiv.html(`<div class="alert alert-danger">${response.message}</div>`);
                 }
             },
             error: function () {
-                alert("保存 Drone wfb 配置失败！");
-            },
-        });
-        // }
-    }
-
-    // 加载 Drone majestic 配置
-    function loadDroneMajesticConfig() {
-        // if ($("#drone-config-majestic").hasClass("active")) {
-        $.get("/load_drone_config/majestic", function (data) {
-            const container = $("#drone-config-container-majestic");
-            container.empty(); // 清空容器
-
-            // 动态生成配置表单
-            for (const [file, content] of Object.entries(data)) {
-                const titel_part = `<h4 class="mt-4 p-1 bg-secondary text-white rounded-2">${file}</h4>`
-                // <hr class="border-primary">`
-                container.append(titel_part);
-                for (const [key, value] of Object.entries(content)) {
-                    const row = `
-                            <div class="mb-3 px-3 row">
-                                <label class="col-form-label col-sm-1 text-start">${key}</label>
-                                <div class="col">
-                                    <input type="text" class="form-control config-input" data-key="${file}.${key}" value="${value}" placeholder="${value}">
-                                </div>
-                            </div>`;
-                    container.append(row);
-                }
-            }
-            const resultDiv = $("#save-result-drone-majestic");
-            resultDiv.html(`<div class="alert alert-success alert-dismissible fade show" role="alert" id="load-result-drone-majestic-success-alert">
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                加载配置成功！
-                                </div>`);
-            // 设置 3 秒后自动消失
-            setTimeout(function () {
-                $('#load-result-drone-majestic-success-alert').alert('close');
-            }, 2000);
-            // resultDiv.html(`<div class="alert alert-success">读取配置成功！</div>`);
-            // alert("加载配置成功！");
-        }).fail(function () {
-            alert("加载 Drone Majestic 配置失败，请手动重新加载！！");
-        });
-        // }
-    }
-
-    // 保存 Drone Majestic 配置
-    function saveDroneMajesticConfig() {
-        // if ($("#drone-config-majestic").hasClass("active")) {
-        const data = {};
-        $(".config-input").each(function () {
-            const key = $(this).data("key");
-            const value = $(this).val();
-            data[key] = value;
-        });
-
-        $.ajax({
-            url: "/save_drone_config/majestic",
-            method: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(data),
-            success: function (response) {
-                const resultDiv = $("#save-result-drone-majestic");
-                if (response.success) {
-                    resultDiv.html(`<div class="alert alert-success alert-dismissible fade show" role="alert" id="save-result-drone-majestic-success-alert">
-                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                        ${response.message}
-                                        </div>`);
-                    // 设置 3 秒后自动消失
-                    setTimeout(function () {
-                        $('#save-result-drone-majestic-success-alert').alert('close');
-                    }, 2000);
-                } else {
-                    resultDiv.html(`<div class="alert alert-danger">${response.message}</div>`);
-                }
-            },
-            error: function () {
-                alert("保存 Drone Majestic 配置失败！");
+                alert(`保存 Drone ${config_name} 配置失败！`);
             },
         });
         // }
@@ -228,8 +148,8 @@ $(document).ready(function () {
 
     // 初始化页面时加载 Drone 配置
     loadGSConfig();
-    loadDroneWfbConfig();
-    loadDroneMajesticConfig();
+    loadDroneConfig("wfb");
+    loadDroneConfig("majestic");
 
     // 加载 gs 配置
     $("#reload-button-gs").on("click", function () {
@@ -243,22 +163,22 @@ $(document).ready(function () {
 
     // 加载 Drone wfb 配置
     $("#reload-button-drone-wfb").on("click", function () {
-        loadDroneWfbConfig();
+        loadDroneConfig("wfb");
     });
 
     // 保存 Drone wfb 配置
     $("#save-button-drone-wfb").on("click", function () {
-        saveDroneWfbConfig();
+        saveDroneConfig("wfb");
     });
 
     // 加载 Drone majestic 配置
     $("#reload-button-drone-majestic").on("click", function () {
-        loadDroneMajesticConfig();
+        loadDroneConfig("majestic");
     });
 
     // 保存 Drone majestic 配置
     $("#save-button-drone-majestic").on("click", function () {
-        saveDroneMajesticConfig();
+        saveDroneConfig("majestic");
     });
 
     // 监听标签页切换事件
